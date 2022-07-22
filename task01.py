3bbc261c20f4e31e28db689fcfa2f1d08e9b03f9
LeiS <84139670+leishi23@users.noreply.github.com>
2022-07-22 02:16:30 -0400

    Add files via upload

--- task01.py
+++ task01.py
@@ -8,7 +8,6 @@ import queue
 import datetime
 import carla
 import random
-from IPython.display import display, clear_output
 
 
 try:
@@ -51,36 +50,8 @@ def process_imu(data, imu_angular_vel_queue, imu_acceleration_queue):
     imu_acceleration_queue.put(data.accelerometer)
 
 
-def process_gnss(data):
-    lat = data.latitude
-    long = data.longitude
-    alt = data.altitude
-    print(2 * "\n")
-    print('position')
-    print(lat)
-    print(long)
-    print(alt)
-
-
-def process_lidar(data):
-    distance = []
-    # np.frombuffer is good while np.array bad, because the data here is a buffer
-    i = np.frombuffer(data.raw_data, dtype=np.dtype('f4'))
-    i2 = np.reshape(i, (int(i.shape[0] / 4), 4))
-    i3 = i2[:, :3]
-    for i in range(i3.shape[0]):
-        temp = pow(pow(i3[i][0], 2) + pow(i3[i][1], 2) + pow(i3[i][2], 2), 0.5)
-        distance.append(temp)
-
-    nearest_dist = min(distance)
-    index = distance.index(nearest_dist)
-    print(2 * "\n")
-    print('lidar')
-    print(i3)
-    if nearest_dist < 3:
-        print("Obstacle!")
-        print(nearest_dist)
-        print((index, len(distance)))
+def process_lidar(data, lidar_queue):
+    lidar_queue.put(data)
 
 
 try:
@@ -115,11 +86,6 @@ try:
     actor_list.append(ego_vehicle)
     print('created ego_%s' % ego_vehicle.type_id)
 
-    # set the spectator
-    spectator = world.get_spectator()
-    spectator.set_transform(
-    carla.Transform(ego_vehicle.get_location() + carla.Location(z=25), carla.Rotation(pitch=-90)))
-
     for _ in range(0, 10):
         # This time we are using try_spawn_actor. If the spot is already
         # occupied by another object, the function will return None.
@@ -134,7 +100,7 @@ try:
 
     # Create a transform to place the camera on top of the vehicle
     camera_transform = carla.Transform(carla.Location(x=0.5, z=2.5))
-    lidar_transform = carla.Transform(carla.Location(x=1.0, z=1.8))
+    lidar_transform = carla.Transform(carla.Location(x=0.5, z=1.5))
 
     # We create sensors through a blueprint that defines its properties
     camera_bp = world.get_blueprint_library().find('sensor.camera.rgb')
@@ -159,37 +125,39 @@ try:
     camera01 = world.spawn_actor(camera_bp, camera_transform, attach_to=ego_vehicle)
     camera02 = world.spawn_actor(camera_dp, camera_transform, attach_to=ego_vehicle)
     IMU = world.spawn_actor(imu_bp, camera_transform, attach_to=ego_vehicle)
-    GNSS = world.spawn_actor(gnss_bp, camera_transform, attach_to=ego_vehicle)
     lidar = world.spawn_actor(lidar_bp, lidar_transform, attach_to=ego_vehicle)
 
     actor_list.append(camera01)
     actor_list.append(camera02)
     actor_list.append(IMU)
-    actor_list.append(GNSS)
     actor_list.append(lidar)
 
     # print('created %s' % camera01.type_id)
     # print('created %s' % camera02.type_id)
     # print('created %s' % IMU.type_id)
-    # print('created %s' % GNSS.type_id)
     # print('created %s' % lidar.type_id)
 
     # The sensor data will be saved in thread-safe Queues
     rgb_image_queue = queue.Queue(maxsize=1)
     dp_image_queue = queue.Queue(maxsize=1)
-    imu_angular_vel_queue = queue.Queue(maxsize=1)
-    imu_acceleration_queue = queue.Queue(maxsize=1)
     rgb_freq_message = queue.Queue()
     dp_freq_message = queue.Queue()
+    imu_angular_vel_queue = queue.Queue(maxsize=1)
+    imu_acceleration_queue = queue.Queue(maxsize=1)
+    location_queue = queue.Queue(maxsize=1)
+    vel_queue = queue.Queue(maxsize=1)
+    lidar_queue = queue.Queue(maxsize=1)
+
     count = 0
     freq_count = 0
     start_time = datetime.datetime.now()
 
     # camera01.listen(lambda data: process_img(data, rgb_image_queue, rgb_freq_message))
     # camera02.listen(lambda data: process_dp(data, dp_image_queue, dp_freq_message))
-    IMU.listen(lambda data: process_imu(data, imu_angular_vel_queue, imu_acceleration_queue))
+    # IMU.listen(lambda data: process_imu(data, imu_angular_vel_queue, imu_acceleration_queue))
+    lidar.listen(lambda data: process_lidar(data, lidar_queue))
 
-    # initial plot part
+    ############################### initial plot part #############################
     # RGB & Depth camera
     # fig, ax = plt.subplots()
     # fig_dp, ax_dp = plt.subplots()
@@ -199,33 +167,58 @@ try:
     # ax.set_title('RGB')
     # l_dp = ax_dp.imshow(dp_array, cmap='gray', interpolation='nearest', vmin=0, vmax=255)
     # ax_dp.set_title('Depth')
+
     # IMU acceleration and angular velocity
-    plt.ion()
-    fig_imu, ((ax_wx, ax_wy), (ax_wz, ax_ax), (ax_ay, ax_az)) = plt.subplots(nrows=3, ncols=2)
-    imu_time = 0
-    imu_x = list()
-    wx = list()
-    wy = list()
-    wz = list()
-    ax = list()
-    ay = list()
-    az = list()
-    ax_wx.set_title('angular x-velocity', fontsize=10)
-    ax_wy.set_title('angular y-velocity', fontsize=10)
-    ax_wz.set_title('angular z-velocity', fontsize=10)
-    ax_ax.set_title('x-acceleration', fontsize=10)
-    ax_ay.set_title('y-acceleration', fontsize=10)
-    ax_az.set_title('z-acceleration', fontsize=10)
-    fig_imu.tight_layout()
+    # plt.ion()
+    # fig_imu, ((ax_wx, ax_wy), (ax_wz, ax_ax), (ax_ay, ax_az)) = plt.subplots(nrows=3, ncols=2)
+    # imu_time = 0
+    # imu_x = list()
+    # wx = list()
+    # wy = list()
+    # wz = list()
+    # ax = list()
+    # ay = list()
+    # az = list()
+    # ax_wx.set_title('angular x-velocity')
+    # ax_wy.set_title('angular y-velocity')
+    # ax_wz.set_title('angular z-velocity')
+    # ax_ax.set_title('x-acceleration')
+    # ax_ay.set_title('y-acceleration')
+    # ax_az.set_title('z-acceleration')
+    # fig_imu.tight_layout()
+
+    # position & velocity
+    # plt.ion()
+    # fig_get, ((ax_px, ax_py, ax_pz), (ax_vx, ax_vy, ax_vz)) = plt.subplots(nrows=2, ncols=3)
+    # get_time = 0
+    # get_x = list()
+    # px = list()
+    # py = list()
+    # pz = list()
+    # vx = list()
+    # vy = list()
+    # vz = list()
+    # ax_px.set_title('position-x', fontsize=10)
+    # ax_py.set_title('position-y', fontsize=10)
+    # ax_pz.set_title('position-z', fontsize=10)
+    # ax_vx.set_title('x-velocity', fontsize=10)
+    # ax_vy.set_title('y-velocity', fontsize=10)
+    # ax_vz.set_title('z-velocity', fontsize=10)
+    # fig_get.tight_layout()
 
     # update the plot data
     while world is not None:
+        location_queue.put(ego_vehicle.get_location())
+        vel_queue.put(ego_vehicle.get_velocity())
         try:
             # Get the data once it's received.
             # image_data = rgb_image_queue.get(True)
             # dp_data = dp_image_queue.get(True)
-            angular_vel_data = imu_angular_vel_queue.get(True)
-            acceleration_data = imu_acceleration_queue.get(True)
+            # angular_vel_data = imu_angular_vel_queue.get(True)
+            # acceleration_data = imu_acceleration_queue.get(True)
+            location_data = location_queue.get(True)
+            vel_data = vel_queue.get(True)
+            lidar_data = lidar_queue.get(True)
         except queue.Empty:
             print("[Warning] Some sensor data has been missed")
             continue
@@ -252,42 +245,88 @@ try:
         # l_dp.set_data(dp_array)
 
         # IMU: angular vel & acceleration
-        imu_time += 1
-        angular_vel_x_array = np.array(angular_vel_data.x)
-        angular_vel_y_array = np.array(angular_vel_data.y)
-        angular_vel_z_array = np.array(angular_vel_data.z)
-        acceleration_x_array = np.array(acceleration_data.x)
-        acceleration_y_array = np.array(acceleration_data.y)
-        acceleration_z_array = np.array(acceleration_data.z)
-        imu_x.append(imu_time)
-        wx.append(angular_vel_x_array)
-        wy.append(angular_vel_y_array)
-        wz.append(angular_vel_z_array)
-        ax.append(acceleration_x_array)
-        ay.append(acceleration_y_array)
-        az.append(acceleration_z_array)
-        if imu_time > 6:
-            ax_wx.set_xlim(imu_time-5, imu_time)
-            # ax_wx.cla()
-            ax_wy.set_xlim(imu_time-5, imu_time)
-            # ax_wy.cla()
-            ax_wz.set_xlim(imu_time-5, imu_time)
-            # ax_wz.cla()
-            ax_ax.set_xlim(imu_time-5, imu_time)
-            # ax_ax.cla()
-            ax_ay.set_xlim(imu_time-5, imu_time)
-            # ax_ay.cla()
-            ax_az.set_xlim(imu_time-5, imu_time)
-            # ax_az.cla()
-        ax_wx.scatter(imu_time, angular_vel_x_array)
-        ax_wy.scatter(imu_time, angular_vel_y_array)
-        ax_wz.scatter(imu_time, angular_vel_z_array)
-        ax_ax.scatter(imu_time, acceleration_x_array)
-        ax_ay.scatter(imu_time, acceleration_y_array)
-        ax_az.scatter(imu_time, acceleration_z_array)
-        # display(fig_imu)
-        # clear_output(wait=True)
-        plt.show()
+        # imu_time += 1
+        # angular_vel_x_array = np.array(angular_vel_data.x)
+        # angular_vel_y_array = np.array(angular_vel_data.y)
+        # angular_vel_z_array = np.array(angular_vel_data.z)
+        # acceleration_x_array = np.array(acceleration_data.x)
+        # acceleration_y_array = np.array(acceleration_data.y)
+        # acceleration_z_array = np.array(acceleration_data.z)
+        # imu_x.append(imu_time)
+        # wx.append(angular_vel_x_array)
+        # wy.append(angular_vel_y_array)
+        # wz.append(angular_vel_z_array)
+        # ax.append(acceleration_x_array)
+        # ay.append(acceleration_y_array)
+        # az.append(acceleration_z_array)
+        # if imu_time > 6:
+        #     ax_wx.set_xlim(imu_time-5, imu_time)
+        #     ax_wy.set_xlim(imu_time-5, imu_time)
+        #     ax_wz.set_xlim(imu_time-5, imu_time)
+        #     ax_ax.set_xlim(imu_time-5, imu_time)
+        #     ax_ay.set_xlim(imu_time-5, imu_time)
+        #     ax_az.set_xlim(imu_time-5, imu_time)
+        # ax_wx.scatter(imu_time, angular_vel_x_array)
+        # ax_wy.scatter(imu_time, angular_vel_y_array)
+        # ax_wz.scatter(imu_time, angular_vel_z_array)
+        # ax_ax.scatter(imu_time, acceleration_x_array)
+        # ax_ay.scatter(imu_time, acceleration_y_array)
+        # ax_az.scatter(imu_time, acceleration_z_array)
+        # # display(fig_imu)
+        # # clear_output(wait=True)
+        # plt.show()
+
+        # position & velocity
+        # get_time += 1
+        # position_x_array = np.array(location_data.x)
+        # position_y_array = np.array(location_data.y)
+        # position_z_array = np.array(location_data.z)
+        # vel_x_array = np.array(vel_data.x)
+        # vel_y_array = np.array(vel_data.y)
+        # vel_z_array = np.array(vel_data.z)
+        # get_x.append(get_time)
+        # px.append(position_x_array)
+        # py.append(position_y_array)
+        # pz.append(position_z_array)
+        # vx.append(vel_x_array)
+        # vy.append(vel_y_array)
+        # vz.append(vel_z_array)
+        # if get_time > 6:
+        #     ax_px.set_xlim(get_time-5, get_time)
+        #     ax_py.set_xlim(get_time-5, get_time)
+        #     ax_pz.set_xlim(get_time-5, get_time)
+        #     ax_vx.set_xlim(get_time-5, get_time)
+        #     ax_vy.set_xlim(get_time-5, get_time)
+        #     ax_vz.set_xlim(get_time-5, get_time)
+        # ax_px.scatter(get_time, position_x_array)
+        # ax_py.scatter(get_time, position_y_array)
+        # ax_pz.scatter(get_time, position_z_array)
+        # ax_vx.scatter(get_time, vel_x_array)
+        # ax_vy.scatter(get_time, vel_y_array)
+        # ax_vz.scatter(get_time, vel_z_array)
+        # plt.show()
+
+        # lidar
+        distance = []
+        p_cloud_size = len(lidar_data)
+        p_cloud = np.copy(np.frombuffer(lidar_data.raw_data, dtype=np.dtype('f4')))
+        p_cloud = np.reshape(p_cloud, (p_cloud_size, 4))
+        angle = lidar_data.horizontal_angle
+
+        # Point cloud in lidar sensor space array of shape (p_cloud_size, 3).
+        local_lidar_points = np.array(p_cloud[:, :3])
+
+        for i in range(p_cloud_size):
+            temp = pow(pow(local_lidar_points[i][0], 2) + pow(local_lidar_points[i][1], 2)
+                       + pow(local_lidar_points[i][2], 2), 0.5)
+            distance.append(temp)
+
+        nearest_dist = min(distance)
+        if nearest_dist < 3:
+            print("Obstacle!")
+            print(nearest_dist)
+            print(angle)
+            print("\n")
 
         plt.pause(0.1)
         # count += 1
@@ -305,8 +344,10 @@ try:
         #     with dp_freq_message.mutex:
         #         dp_freq_message.queue.clear()
 
-        # GNSS.listen(lambda data: process_gnss(data))
-        # lidar.listen(lambda data: process_lidar(data))
+        # set the spectator
+        spectator = world.get_spectator()
+        spectator.set_transform(
+            carla.Transform(ego_vehicle.get_location() + carla.Location(z=25), carla.Rotation(pitch=-90)))
 
 finally:
     print("Over")
